ec771ce08f486d2b2c8f68dc8fd9e16ae97e318f
nityamula@gmail.com <nityamula@gmail.com>
2021-05-31 17:25:56 -0500

    Added authentication authorization and user profile

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,16 @@
 // const dotenv = require('dotenv')
 // const products = require('./data/products')
 
-import express from 'express';
-import dotenv from 'dotenv';
-import colors from 'colors';
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 // import products from './data/products.js'
+import bodyParser from 'body-parser'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -18,21 +20,15 @@ connectDB()
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send('API is running....')
+  res.send('API is running....')
 })
 
 app.use('/api/products', productRoutes)
-
-// app.get('/api/products', (req, res) => {
-//     res.json(products)
-// })
-
-// app.get('/api/products/:id', (req, res) => {
-//     const product = products.find(p => p._id === req.params.id)
-//     res.json(product)
-// })
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
@@ -41,7 +37,8 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 
 app.listen(
-    PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
-    )
-
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
